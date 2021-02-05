6a1dae1d0bf1b160d62f2a7c4452a3bf29e36855
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-04 23:40:23 -0500

    Apollo server setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    info: () => 'This is the API of Hackernews Clone'
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server
+  .listen()
+  .then(({ url }) => console.log(`Server is running on ${url}`));
