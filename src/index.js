4dc1a3e575a7a43df40c61239e1c79d1fdaf6966
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-07 00:53:49 -0500

    correct type

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, PubSub } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
@@ -9,6 +9,7 @@ const Subscription = require('./resolvers/Subscription');
 const fs = require('fs');
 const path = require('path');
 const { getUserId } = require('./utils');
+const { PubSub } = require('apollo-server');
 
 const prisma = new PrismaClient();
 const pubsub = new PubSub();
@@ -18,12 +19,10 @@ const resolvers = {
   Query,
   Mutation,
   Subscription,
-  Vote,
   User,
-  Link
+  Link,
+  Vote,
 };
-
-
 
 const server = new ApolloServer({
   typeDefs: readDefs,
