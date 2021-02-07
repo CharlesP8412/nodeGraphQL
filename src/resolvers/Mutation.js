138d553760e937ddbd8c204f7bf089a1256636d8
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-06 23:45:06 -0500

    Test Auth

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,8 +1,8 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { APP_SECRET, getUserId } = require('../utils')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET, getUserId } = require('../utils');
 
-const signup = async (parent, args, context, info) => {
+const signup = async(parent, args, context, info) => {
   const password = await bcrypt.hash(args.password, 10);
 
   const user = await context.prisma.user.create({ data: { ...args, password } });
@@ -15,23 +15,23 @@ const signup = async (parent, args, context, info) => {
   };
 };
 
-const login = async (parent, args, context, info) => {
-  const user = await context.prisma.user.findUnique({ where: { email: args.email } })
+const login = async(parent, args, context, info) => {
+  const user = await context.prisma.user.findUnique({ where: { email: args.email } });
   if (!user) {
-    throw new Error('No such user found')
+    throw new Error('No such user found');
   }
 
-  const valid = await bcrypt.compare(args.password, user.password)
+  const valid = await bcrypt.compare(args.password, user.password);
   if (!valid) {
-    throw new Error(`Invalid password`)
-  };
+    throw new Error(`Invalid password`);
+  }
 
   const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
-  return { token, user, }
+  return { token, user, };
 };
 
-const post = async (parent, args, context, info) => {
+const post = async(parent, args, context, info) => {
   const { userId } = context;
 
   return await context.prisma.linkcreate({
@@ -40,7 +40,7 @@ const post = async (parent, args, context, info) => {
       description: args.description,
       postedBy: { connect: { id: userId } },
     }
-  })
+  });
 };
 
 module.exports = {
