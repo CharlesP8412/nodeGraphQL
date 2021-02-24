3aed7c6d2360ba9b97735c01dc7e5be971a5439b
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-23 21:21:18 -0500

    Linting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
@@ -9,7 +9,6 @@ const Subscription = require('./resolvers/Subscription');
 const fs = require('fs');
 const path = require('path');
 const { getUserId } = require('./utils');
-const { PubSub } = require('apollo-server');
 
 const prisma = new PrismaClient();
 const pubsub = new PubSub();
